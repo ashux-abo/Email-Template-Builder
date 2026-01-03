cb3f072946a727f5fe73d5879d0ad6543fec633f
ashux-abo <dulayashley12@gmail.com>
2026-01-03 21:43:23 +0800

    Update route.ts

--- src/app/api/contacts/route.ts
+++ src/app/api/contacts/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import connectDB from "../../../../lib/db";
-import Contact from "@/models/Contact";
+import connectDB from "../../../lib/db";
+import Contact from "../../../models/Contact";
 import { getUserFromRequest } from "../../../lib/auth";
 
 // GET - Fetch all contacts for the current user
